03574e2da5728487b08c3a845a843ef6561fd390
NagarajuKurakula1256 <42274258+NagarajuKurakula1256@users.noreply.github.com>
2024-02-21 10:29:53 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -3,6 +3,6 @@ public class Helloworld
 
 public static void main(String[] args)
 {
-System.out.println("Welcome to Jenkins sssthis is java project but not .net with webhooks test");
+System.out.println("Welcsssssssssssome to Jenkins sssthis is java project but not .net with webhooks test");
 }
 }
